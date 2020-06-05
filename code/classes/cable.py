1004af652b2c8cd396e75368088a18a64cbe113a
Simon <simonveeden@hotmail.com>
2020-06-05 10:31:27 +0200

    Initialized cable class

---
+++ code/classes/cable.py
@@ -0,0 +1,10 @@
+class Cable():
+    def __init__(self, house, battery):
+        self.house = house
+        self.battery = battery
+        self.path = []
+
+    def calc_length(self):
+        """ Returns length of cable """
+        
+        return len(path)
---
+++ code/classes/house.py
@@ -0,0 +1,11 @@
+class House():
+    def __init__(self, uid, location, output):
+        self.id = uid
+        self.location = location
+        self.output = output
+        self.cable = None
+
+    def get_location(self):
+        """ Returns location """
+    
+        return location
