602ec9186b14eca3e9cce9942bb173580dcf9e0b
Simon <simonveeden@hotmail.com>
2020-06-15 16:31:19 +0200

    draw implented for new datastructure

--- code/visualization/draw.py
+++ code/visualization/draw.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy
 
-def plot(district):
+def plot_cables(district):
     """ 
     Draw grid with houses, batteries and connections
     """
@@ -15,19 +15,16 @@ def plot(district):
 
     # loop through batteries
     for battery in district.get_batteries():
-        x, y = battery.get_location()
+        x, y = battery.location
         plt.plot(x, y, 's', label = f'battery{battery.id}', markersize=10, \
             color=color[battery.id])
-
-    # coordinate lists for houses
-    x_houses = []
-    y_houses = []
+        # plt.text(x + 0.4, y - 0.2, f"{battery.usage:.1f}", fontsize=8)
 
     # loop through houses
     for house in district.get_houses():
-        x, y = house.get_location()
-        x_houses.append(x)
-        y_houses.append(y)
+        x, y = house.location
+        plt.plot(x, y, 'kp', markersize=7)
+        # plt.text(x + 0.2, y + 0.2, f"{house.output:.0f}", fontsize=8)
 
     # loop through cables
     for cable in district.cables:
@@ -43,10 +40,33 @@ def plot(district):
             y_path.append(y)
       
         # add cable path to plot
-        plt.plot(x_path, y_path, "-", color=color[cable.battery.id])
+        plt.plot(x_path, y_path, "-", color=color[cable.battery.id], alpha=0.3)
 
     # plot district    
-    plt.plot(x_houses, y_houses, 'kp', label = 'house', markersize=7)
+    ax.set_xticks(numpy.arange(0, 51, 1), minor=True)
+    ax.set_yticks(numpy.arange(0, 51, 1), minor=True)
+    ax.grid(which='minor', alpha=0.2)
+    plt.legend()
+    plt.show()
+
+def plot_connections(district):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1) 
+    plt.title(f'District{district.id}')
+
+    color = {0: "blue", 1:"red" ,2:"yellow",3:"cyan", 4:"magenta"} 
+
+    # loop through batteries
+    for battery in district.batteries:
+        x, y = battery.location
+        plt.plot(x, y, 'ks', label = f'battery{battery.id}', color=color[battery.id], markersize=10)
+
+        for house in district.connections[battery.id]:             
+            x, y = house.location
+            plt.plot(x, y, 'p', color=color[battery.id], markersize=7, alpha=0.5)
+
+    # plot district  
     ax.set_xticks(numpy.arange(0, 51, 1), minor=True)
     ax.set_yticks(numpy.arange(0, 51, 1), minor=True)
     ax.grid(which='minor', alpha=0.2)
