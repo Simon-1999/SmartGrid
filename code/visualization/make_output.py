e1541ad5a8bd2d73e8cc26e6d64976d9066b469d
Noah Susanne van de Bunt <Noah@Noahs-Macbook-Pro-4.local>
2020-06-19 10:19:37 +0200

    output method

---
+++ code/visualization/make_output.py
@@ -0,0 +1,48 @@
+import json
+#from .algorithms.shareddistrict import District
+
+def output_doc(district, shared):
+    """
+    Writes a District object to a .json output file. 
+    """
+    # create a list to store all the data in
+    data = []
+
+    # start with the first item in the dictionary; general district info
+    district_info = {}
+    district_info['district'] = district.id
+    if shared:
+        district_info['shared-costs'] = district.calc_connection_costs()['total']
+    else:
+        district_info['own-costs'] = district.calc_connection_costs()['total']
+
+    data.append(district_info)
+
+    # loop through every battery in the district
+    for battery in district.get_batteries():
+        battery_data = {}
+
+        # create general info for each battery
+        battery_data['location'] = battery.location
+        battery_data['capacity'] = battery.capacity
+        battery_data['houses'] = []
+
+        data.append(battery_data)
+
+        # loop through every house in the district
+        for house in district.connections[battery.id]:
+
+            # store house information in dictionary
+            house_info = {}
+
+            house_info['location'] = house.location
+            house_info['output'] = house.output
+            house_info['cables'] = district.cables[house.id]
+
+            # add house dict to battery
+            battery_data['houses'].append(house_info)
+
+
+    # write everything to output file
+    with open("output.json", "w+") as outfile:
+        json.dump(data, outfile)
