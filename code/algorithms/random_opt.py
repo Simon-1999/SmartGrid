2b7cb10d01baa66f72ce750833c454348347c2fe
HendrikScheeres <hscheeresyt@gmail.com>
2020-06-23 17:14:47 +0200

    added documentation

--- code/algorithms/random_opt.py
+++ code/algorithms/random_opt.py
@@ -1,11 +1,40 @@
 import random
 from .algorithm import Algorithm
 
+"""Finds a solution by randomly connecting houses to the nearest battery with free capacity
+
+The algortihm randomly connects each house to the nearest battery with remaining capacity. 
+It repeats this until a valid configuration is found and returns the district with this configuration.
+"""
+
 class RandomOptimize(Algorithm):
+    """ Randomly connects houses to the nearest batteries in a district and returns the district once a valid solution is found.
+
+    The algorithm shuffles the list of houses in the district randomly, loops through the list of houses and creates 
+    a connection between each house and the nearest battery with capacity left from the batteries in the district.
+    After checking if the configuration is valid, it either sets the district cables and returns the valid district 
+    or it resets the district and runs again.
+
+    Methods
+    ----------
+    run()
+        Runs the algorithm
+
+    assign_connections()
+        Assigns the individual houses to a battery
+
+    get_nearest_free_battery()
+        Returns the nearest free battery to the house
+    
     """
-    Finds a solution by connecting houses to the nearest battery with free capacity
-    """
-    def run(self):    
+    def run(self): 
+        """Runs the Randomize algorithm.
+
+        Returns
+        ----------
+        District object
+            A valid optimized district configuration
+        """   
 
         while True:
 
@@ -28,8 +57,7 @@ class RandomOptimize(Algorithm):
 
 
     def assign_connections(self):
-        """
-        Assign houses to batteries
+        """Assign houses to batteries and creates connections
         """
 
         # loop through all houses
@@ -47,6 +75,10 @@ class RandomOptimize(Algorithm):
     def get_nearest_free_battery(self, house):
         """
         Calculates which battery in the list is the nearest to the given house
+
+        Returns
+        ----------
+        Battery object
         """
 
         possible_batteries = self.district.get_possible_batteries(house)
